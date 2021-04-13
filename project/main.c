b92c7dcfb9a1ff43418b78a224aabcace60a76fc
erikdao <cuong@neuraltalks.io>
2021-04-13 17:07:17 +0200

    Add: sample JPEG image

---
+++ project/main.c
@@ -0,0 +1,9 @@
+/**
+ * Parallel Edge Detection for High-Resolution Image
+ * Course Project - SF2568
+ * Author: Cuong Dao, Donggyun Park
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
